7298c26aa009bc1940fc440267c0c8ad46ca358b
Avisena <37996353+Avisena@users.noreply.github.com>
2025-05-08 16:30:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,11 @@ for i, (user_q, bot_a) in enumerate(reversed(st.session_state.chat_history)):
     with st.chat_message("user", avatar="👤"):
         st.markdown(user_q)
     with st.chat_message("assistant", avatar="🤖"):
-        st.markdown(bot_a)
+        try:
+            st.latex(bot_a)
+        except:
+            st.markdown(bot_a)
+
 
 
 
